49b0849e7f3a4acb689874d74f737f9118bac4e0
iheng <iheng_scau@hotmail.com>
2021-05-30 18:37:18 +0800

    20210530-submit,补充了更简洁的解法

--- src/main/java/org/excode/algorithm/starter/subjects/PowerTwo.java
+++ src/main/java/org/excode/algorithm/starter/subjects/PowerTwo.java
@@ -8,7 +8,28 @@ import org.junit.Test;
  * @date 2021/5/30
  */
 public class PowerTwo {
+
+    /**
+     * 一行简洁, 利用2的n次方的二进制特性进行
+     * n & n-1 按位&的结果为0
+     * 一定的, 2^n只有在第k位为1其他位为0
+     * 而n-1的[k-1,1]位都为0
+     * 另外的思路是
+     * 1.  n &(-n) =n
+     * 2.  2^3mod n ==0
+     * @param n
+     * @return
+     */
     public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n-1)) ==0;
+    }
+
+    /**
+     *  循环取mod的方式判定
+     * @param n
+     * @return
+     */
+    public boolean isPowerOfTwo1(int n) {
         int mod;
 
         if(n==1){
@@ -37,7 +58,7 @@ public class PowerTwo {
         Assert.assertTrue(result);
 
         result = powerTwo.isPowerOfTwo(-16);
-        Assert.assertTrue(result);
+        Assert.assertFalse(result);
 
         result = powerTwo.isPowerOfTwo(3);
         Assert.assertFalse(result);
