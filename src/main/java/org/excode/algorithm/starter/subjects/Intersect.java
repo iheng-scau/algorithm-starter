29fda59ba45ac43c60d8f783687b5a0217f55ccd
iheng <iheng_scau@hotmail.com>
2021-06-05 00:18:04 +0800

    20210605-参考回文串解法

---
+++ src/main/java/org/excode/algorithm/starter/subjects/Intersect.java
@@ -0,0 +1,53 @@
+package org.excode.algorithm.starter.subjects;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author iheng
+ * @date 2021/6/4
+ */
+public class Intersect {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        List<ListNode> reg = new ArrayList<>();
+
+        if(headA == null || headB == null){
+            return null;
+        }
+
+        if(headA.val == headB.val){
+            return headA;
+        }
+
+        while(headA.next!=null){
+            reg.add(headA);
+            headA = headA.next;
+        }
+
+        while(headB.next != null){
+            if(reg.contains(headB)){
+                return headB;
+            }
+            headB = headB.next;
+        }
+
+        return null;
+    }
+
+    @Test
+    public void test(){
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
