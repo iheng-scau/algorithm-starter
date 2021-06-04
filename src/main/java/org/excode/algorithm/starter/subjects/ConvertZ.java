b197a4782604115d079a320b4ff88519dc4144f9
iheng <iheng_scau@hotmail.com>
2021-06-05 00:29:33 +0800

    20210605

---
+++ src/main/java/org/excode/algorithm/starter/subjects/ConvertZ.java
@@ -0,0 +1,11 @@
+package org.excode.algorithm.starter.subjects;
+
+/**
+ * @author iheng
+ * @date 2021/6/5
+ */
+public class ConvertZ {
+    public String convert(String s, int numRows) {
+        return null;
+    }
+}
